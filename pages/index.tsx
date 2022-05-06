69531e5894fe2d51a1a116ffcce3547b738f5c30
Jaehwan Choi <lieblichoi@gmail.com>
2022-05-06 10:55:10 +0900

    Setting

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className='bg-red-500'>
+      <div className='text-gray-300'>
+        test
+      </div>
+    </div>
+  );
+  
+}
+
+export default Home
